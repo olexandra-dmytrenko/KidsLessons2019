b02837d7c99e633ec8ea05bca272694264c4075f
Oleksandra_Dmytrenko <oleksandra_dmytrenko1@epam.com>
2019-04-01 16:27:37 +0300

    Rearranged, renamed and organised the lessons. * Added lesson 3: Filling the array with letters game. * Added lesson 4: Drawing matrix using computer or Raspberry Pi as output.

--- Lesson3_2D_Array_Game/2D_array_quest/__main__.py
+++ Lesson3_2D_Array_Game/2D_array_quest/__main__.py
@@ -1,3 +1,13 @@
+"""
+Kids are to guess all the letters and their position.
+Matrix works as Turing machine. Current letter and the next position are coded.
+To find the second or next current letter
+one should check the position of the following letter and as "current",
+your current letter will be positioned.
+
+As a homework kids are to change the letters and positions
+codding their own words.
+"""
 print("DEBUG: Module '__main__.py' is executing")
 
 from cli import main
