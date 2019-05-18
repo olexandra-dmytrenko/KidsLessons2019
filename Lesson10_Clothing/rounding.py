fbeb97f98d2e3647c3a265b4d9c3637b34f73f8b
Olexandra <olexandra.dmytrenko@gmail.com>
2019-05-18 09:24:35 +0300

    Final tasks. Refactoring

---
+++ Lesson10_Clothing/rounding.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("Яку картинку потрібно відкрити?")
+
+picture_names = ["pic1.tif", "pic2.tif", "pic3.tif"]
+
+index = int(0.8) + int(0.5) + int(0.2)
+
+#print("Відкрийте картинку = " + picture_names[index])
+
---
+++ test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Тест на базові поняття по Python можна пройти тут:
+# https://quizlet.com/_6ntfz3
+
+# for i in range(10):
+#     print(i)
+#
+# for i in range(1, 10):
+#     print(i)
+#
+
+# for i in range(int(3/2), 10, 3):
+#     print(i)
+#
+# while True:
+#     print("Hi")
+#
+# while False:
+#     print("Hi")
+#
+# i = 0
+# while i<10:
+#     print(i)
+
+# i = 10
+# while i >= 10:
+#     print(i)
+
+# i = 0
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+#
+#
+# name = "bad guy"
+# if (name=="mom"):
+#     print("I love you mom")
+# elif (name=="dad"):
+#     print("You're the best, dad!")
+# else:
+#     print("I don't know you, " + name)
+#
+# def output(great):
+#     print(great)
+# output("Hi")
+
+def output(great, name = "body"):
+    print(great + " " + name)
+output("Hi", "Kate")
+
+
